84a52164c2ab5b4eff543ba7ec34f6994fbfa7b1
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-30 13:55:42 -0700

    putting in place the Distribution folder

---
+++ Distribution/Exception.js
@@ -0,0 +1,60 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Exception = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /*---------------------------------------------------------------------------------------------
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      *  Copyright (c) Einar Ingebrigtsen. All rights reserved.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      *  Licensed under the MIT License. See License.txt in the project root for license information.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      *--------------------------------------------------------------------------------------------*/
+
+
+var _cratisJavascriptReflection = require("cratis-javascript-reflection");
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var _message = new WeakMap();
+
+/**
+ * Defines a base for structured exceptions 
+ */
+
+var Exception = exports.Exception = function () {
+  /**
+   * Initializes a new instance of {Exception}
+   * @param {message} Message to include with the exception 
+   */
+  function Exception(message) {
+    _classCallCheck(this, Exception);
+
+    _message.set(this, message);
+  }
+
+  /**
+   * Get the message for the exception
+   */
+
+
+  _createClass(Exception, [{
+    key: "message",
+    get: function get() {
+      if (_message.get(this)) return _message.get(this);
+      return "";
+    }
+
+    /**
+     * Throw an instance of the exception
+     */
+
+  }], [{
+    key: "throw",
+    value: function _throw(messageParameters) {
+      throw new this();
+    }
+  }]);
+
+  return Exception;
+}();
+//# sourceMappingURL=Exception.js.map
---
+++ Distribution/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Exception = require("./Exception");
+
+Object.keys(_Exception).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _Exception[key];
+    }
+  });
+});
+//# sourceMappingURL=index.js.map
