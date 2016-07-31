c85333dcd99d0d708a63b878b06cbfbd5e7568d6
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-31 13:15:42 -0700

    Adding copyright header

--- Specifications/for_Exception/when_throwing_exception_without_specified_message.js
+++ Specifications/for_Exception/when_throwing_exception_without_specified_message.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Einar Ingebrigtsen. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import {Exception} from "../../Source/Exception";
 
 class MyException extends Exception
